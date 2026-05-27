e33eaac0bbed2f69e883f67d92486c0676a4ddcf
SOURABH-MALHOTRA <malhotrasourabh034@gmail.com>
2026-05-27 10:17:03 +0530

    setup github pages

--- my-portfolio/vite.config.js
+++ my-portfolio/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [tailwindcss()],
-  base: '/',   // ✅ Vercel ke liye
+  plugins: [react(), tailwindcss()],
+  base: '/My_Portfolio/',
 })
